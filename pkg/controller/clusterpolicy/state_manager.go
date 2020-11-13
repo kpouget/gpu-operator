e32d7b5691cd72d64f6bcad5917dec23fcad5522
Kevin Pouget <kpouget@redhat.com>
2020-11-13 13:21:38 +0100

    state_manager: avoid accumulating states
    The method `ClusterPolicyController.init()` is called every time
    `ReconcileClusterPolicy.Reconcile` is called, many times.
    
    So `ClusterPolicyController.init()` should not call `addState` each
    time, otherwise `n.controls` and `n.resources` accumulate states.
    
    AFAIU, there is no visible bug caused by this issue, but the
    GPU-operator over-consumes memory and CPU because of it.
    
    Signed-off-by: Kevin Pouget <kpouget@redhat.com>

--- pkg/controller/clusterpolicy/state_manager.go
+++ pkg/controller/clusterpolicy/state_manager.go
@@ -153,25 +153,28 @@ func (n *ClusterPolicyController) init(r *ReconcileClusterPolicy, i *gpuv1.Clust
 	n.rec = r
 	n.idx = 0
 
-	promv1.AddToScheme(r.scheme)
-	secv1.AddToScheme(r.scheme)
-
 	// fetch all nodes and label gpu nodes
 	err = n.labelGPUNodes()
 	if err != nil {
 		return err
 	}
 
-	addState(n, "/opt/gpu-operator/state-driver")
-	addState(n, "/opt/gpu-operator/state-container-toolkit")
-	addState(n, "/opt/gpu-operator/state-driver-validation")
-	addState(n, "/opt/gpu-operator/state-device-plugin")
-	addState(n, "/opt/gpu-operator/state-device-plugin-validation")
-	addState(n, "/opt/gpu-operator/state-monitoring")
-	// if requested, deploy GFD to utilitize Multi-Instance GPU's.
-	if i.Spec.Operator.DeployGFD {
-		addState(n, "/opt/gpu-operator/gpu-feature-discovery")
+	if len(n.controls) == 0 {
+		promv1.AddToScheme(r.scheme)
+		secv1.AddToScheme(r.scheme)
+
+		addState(n, "/opt/gpu-operator/state-driver")
+		addState(n, "/opt/gpu-operator/state-container-toolkit")
+		addState(n, "/opt/gpu-operator/state-driver-validation")
+		addState(n, "/opt/gpu-operator/state-device-plugin")
+		addState(n, "/opt/gpu-operator/state-device-plugin-validation")
+		addState(n, "/opt/gpu-operator/state-monitoring")
+		// if requested, deploy GFD to utilitize Multi-Instance GPU's.
+		if i.Spec.Operator.DeployGFD {
+			addState(n, "/opt/gpu-operator/gpu-feature-discovery")
+		}
 	}
+
 	return nil
 }
 
